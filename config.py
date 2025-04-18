71e1add4012e050bc35ff4cad7c684944220f8f5
Greed1959JR <amarildomsj@gmail.com>
2025-04-18 13:24:29 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+TELEGRAM_BOT_TOKEN = "SEU_TOKEN_AQUI"
+
+# Substitua pelos IDs reais dos grupos onde o bot é admin
+GROUPS = {
+    "vip": -1002600167995,
+    "free": -1002508674229
+}
